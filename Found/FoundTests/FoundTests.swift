3bf3bebc652561e6dccfce6a486e472beccd2d5a
ilomacht <104243469+ilomacht@users.noreply.github.com>
2024-11-23 22:19:12 -0500

    Commit test

---
+++ Found/FoundTests/FoundTests.swift
@@ -0,0 +1,17 @@
+//
+//  FoundTests.swift
+//  FoundTests
+//
+//  Created by Ryan Ye on 11/23/24.
+//
+
+import Testing
+@testable import Found
+
+struct FoundTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
